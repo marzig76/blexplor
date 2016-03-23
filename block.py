23ae4a2da7b69c8c6479d8f399d6ae87776507dd
Mike M <marzig76@gmail.com>
2016-03-22 21:20:45 -0400

    just prting some block header values

--- block.py
+++ block.py
@@ -1,6 +1,32 @@
 import struct
+import datetime
 
-blockfile = '/home/marzig76/.bitcoin/blocks/blk00255.dat'
+blockfile = '/home/marzig76/.bitcoin/blocks/blk00000.dat'
 
-with open(blockfile, 'rb') as blockstream:
-    block = blockstream.read(1024)
+blockstream = open(blockfile, 'rb')
+
+def reverse_bytes(bytes):
+    reversed = ""
+    for i in range(0, len(bytes), 2):
+        reversed += bytes[::-1][i:i+2][::-1]
+    return reversed
+
+
+print 'Magic Number: ', hex(struct.unpack('I', blockstream.read(4))[0])
+print 'Block size: ', struct.unpack('I', blockstream.read(4))[0]
+
+print 'Version: ', struct.unpack('I', blockstream.read(4))[0]
+print 'Prev Hash: ', reverse_bytes(blockstream.read(32).encode("hex"))
+print 'Merkel Root: ', reverse_bytes(blockstream.read(32).encode("hex"))
+print 'time: ', (
+    datetime.datetime.fromtimestamp(
+        struct.unpack('I', blockstream.read(4))[0]
+        ).strftime('%Y-%m-%d %H:%M:%S'))
+
+
+print 'target: ', hex(struct.unpack('I', blockstream.read(4))[0])
+print 'nonce: ', struct.unpack('I', blockstream.read(4))[0]
+
+print 'txcount: ', ord(blockstream.read(1))
+
+#print blockstream.read(32).encode("hex")
