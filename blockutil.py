3eb6a1d4e6e80f2dfbed13896a6e731dee8fb030
Mike M <marzig76@gmail.com>
2016-03-23 22:01:33 -0400

    reverse_byte function was unnecessary

--- blockutil.py
+++ blockutil.py
@@ -1,16 +1,16 @@
 import struct
 
-
-def reverse_bytes(bytes):
-    reversed = ""
-    for i in range(0, len(bytes), 2):
-        reversed = bytes[i:i+2] + reversed
-    return reversed
-
-
-def parse_int(blockstream):
-    return struct.unpack('I', blockstream.read(4))[0]
+def parse_int(blockstream, bytes):
+    if bytes == 2:
+        format = 'H'
+    elif bytes == 4:
+        format = 'I'
+    elif bytes == 8:
+        format = 'Q'
+    else:
+        return False
+    return struct.unpack(format, blockstream.read(bytes))[0]
 
 
 def parse_hash(blockstream):
-    return reverse_bytes(blockstream.read(32).encode("hex"))
+    return blockstream.read(32)[::-1].encode("hex")
