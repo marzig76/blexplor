6e43079564b2c48cbca77934ce066a22c3c69f74
Mike M <marzig76@gmail.com>
2016-03-23 22:32:34 -0400

    just started parsing txs.. much more work needed

--- tx.py
+++ tx.py
@@ -1,10 +1,18 @@
-
+from blockutil import *
 
 class tx(object):
 
     version = ''
-    tx_in count = ''
+    tx_in_count = ''
     tx_in = ''
-    tx_out count = ''
+    tx_out_count = ''
     tx_out = ''
     lock_time = ''
+
+    def __init__(self, blockstream):
+        self.version = parse_int(blockstream, 4)
+        self.tx_in_count = compact_size(blockstream)
+
+
+    def __str__(self):
+        return 'finish me!'
