7cf82a7b9e0bb97597cacd70d0cd15885e8d5d24
Mike M <marzig76@gmail.com>
2016-03-23 22:02:39 -0400

    added __str__ function

--- block.py
+++ block.py
@@ -1,3 +1,5 @@
+import datetime
+from blockutil import *
 
 
 class block(object):
@@ -5,7 +7,7 @@ class block(object):
     magic_number = ''
     block_size = ''
     version = ''
-    prev_hash ''
+    prev_hash = ''
     merkel_root = ''
     time = ''
     target = ''
@@ -13,15 +15,28 @@ class block(object):
     txcount = ''
 
     def __init__(self, blockstream):
-        self.magic_number = hex(parse_int(blockstream))
-        self.block_size = parse_int(blockstream)
-        self.version = parse_int(blockstream)
+        self.magic_number = parse_int(blockstream, 4)
+        self.block_size = parse_int(blockstream, 4)
+        self.version = parse_int(blockstream, 4)
         self.prev_hash = parse_hash(blockstream)
         self.merkel_root = parse_hash(blockstream)
-        self.time = (
-            datetime.datetime.fromtimestamp(
-                parse_int(blockstream)
-                ).strftime('%Y-%m-%d %H:%M:%S'))
-        self.target = ''
-        self.nonce = ''
-        self.txcount = ''
+        self.time = parse_int(blockstream, 4)
+        self.target = parse_int(blockstream, 4)
+        self.nonce = parse_int(blockstream, 4)
+        self.txcount = ord(blockstream.read(1))
+
+    def __str__(self):
+        return (
+            'Magic Number:\t' + hex(self.magic_number) +
+            '\nBlock Size:\t' + str(self.block_size) +
+            '\nVersion:\t' + str(self.version) +
+            '\nPrevious Hash:\t' + self.prev_hash +
+            '\nMerkel Root:\t' + self.merkel_root +
+            '\nTime:\t' + (
+                datetime.datetime.fromtimestamp(
+                    self.time
+                ).strftime('%Y-%m-%d %H:%M:%S')) +
+            '\nTarget:\t' + hex(self.target) +
+            '\nNonce:\t' + str(self.nonce) +
+            '\nTransactions:\t' + str(self.txcount)
+        )
