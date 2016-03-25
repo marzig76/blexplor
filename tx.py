9a5311914c777ad6cc3bc3be6a97b10e93af0135
Mike M <marzig76@gmail.com>
2016-03-24 21:19:42 -0400

    added support for tx inputs and outputs

--- tx.py
+++ tx.py
@@ -1,18 +1,45 @@
 from blockutil import *
+from tx_input import tx_input
+from tx_output import tx_output
 
 class tx(object):
 
-    version = ''
-    tx_in_count = ''
-    tx_in = ''
-    tx_out_count = ''
-    tx_out = ''
-    lock_time = ''
 
     def __init__(self, blockstream):
         self.version = parse_int(blockstream, 4)
-        self.tx_in_count = compact_size(blockstream)
+        self.tx_input_count = compact_size(blockstream)
+        self.tx_inputs = []
 
+        for i in range(0, self.tx_input_count):
+            self.tx_inputs.append(tx_input(blockstream))
+
+        self.tx_output_count = compact_size(blockstream)
+        self.tx_outputs = []
+
+        for i in range(0, self.tx_output_count):
+            self.tx_outputs.append(tx_output(blockstream))
+
+        self.lock_time = parse_int(blockstream, 4)
 
     def __str__(self):
-        return 'finish me!'
+        txstring = (
+            '\n-------------------------------------' +
+            '\nTx Version:\t' + str(self.version) +
+            '\nInput Count:\t' + str(self.tx_input_count) +
+            '\nInputs:'
+        )
+
+        for i in self.tx_inputs:
+            txstring += str(i)
+
+        txstring += (
+            '\nOutput Count:\t' + str(self.tx_output_count) +
+            '\nOutputs:'
+        )
+
+        for i in self.tx_outputs:
+            txstring += str(i)
+
+        txstring += '\nLock Time:\t' + str(self.lock_time)
+        txstring += '\n-------------------------------------'
+        return txstring
