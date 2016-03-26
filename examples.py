f76ca9a969814d2003c316811f4ca59e51af8143
Mike Marzigliano <marzig76@gmail.com>
2016-03-26 16:59:28 -0400

    added a script example

--- examples.py
+++ examples.py
@@ -1,4 +1,5 @@
 from block import block
+from script import script
 
 blockfile = '/home/marzig76/.bitcoin/blocks/blk00000.dat'
 
@@ -6,3 +7,12 @@ blockstream = open(blockfile, 'rb')
 
 parsed_block = block(blockstream)
 print parsed_block
+
+# script examples
+# orig              76a914fb96940932cc00274cf6ce423623e0cb0aa32ccf88ac
+# 0x4c OP_PUSHDATA1 76a94c14fb96940932cc00274cf6ce423623e0cb0aa32ccf88ac
+# 0x4d OP_PUSHDATA2 76a94d0100deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef88ac
+# 0x4d OP_PUSHDATA2 76a94d0001ff88ac
+# 0x4e OP_PUSHDATA4 76a94e00000001ff88ac
+pk_script = script('76a94e00000001ff88ac'.decode('hex'))
+print pk_script
