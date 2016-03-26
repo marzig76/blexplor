e0603f98265f170d50d138d4d22fec96a4afc611
Mike Marzigliano <marzig76@gmail.com>
2016-03-26 16:57:07 -0400

    parse out pubKeyScript so its human readable

--- script.py
+++ script.py
@@ -1,27 +1,73 @@
+import struct
+from opcode import opcode
 
-# config OP Codes
-op_codes = {}
-op_codes['OP_FALSE'] = 0x00
-op_codes['OP_NOP'] = 0x61
-op_codes['OP_IF'] = 0x63
-op_codes['OP_ELSE'] = 0x67
-op_codes['OP_ENDIF'] = 0x68
 
-# input
-value = 0x61
+class script(object):
 
-# push data to stack
-if 0x01 <= value <= 0x4b:
-    data = stream.read(value)
-elif value == 0x4c:
-    data = stream.read(stream.read(1))
-elif value == 0x4d:
-    data = stream.read(stream.read(2))
-elif value == 0x4e:
-    data = stream.read(stream.read(4))
-elif value in op_codes.values():
-    for key in op_codes:
-        if op_codes[key] == value:
-            print key
-else:
-    print False
+    opcodes = opcode().opcodes
+
+    def __init__(self, bytes):
+        self.bytes = bytes
+
+    def __str__(self):
+
+        # set up some counters
+        byte_count = 0
+        skip_count = 0
+        print_skip = 0
+        stack = ''
+
+        # build the script
+        for i in self.bytes:
+            byte = ord(i)
+
+            # Either find a corresponding opcode
+            # or read the number of appropriate bytes of data.
+            # If just reading bytes, skip checking them as opcodes
+            if byte == 0x6a:
+                # 0x6a (OP_RETURN) the rest of the script is just data.
+                # skip it all
+                skip_count = len(self.bytes)
+            elif skip_count > 0:
+                if print_skip == 0:
+                    stack += i.encode('hex')
+                else:
+                    print_skip -= 1
+                skip_count -= 1
+                if skip_count == 0:
+                    stack += ' '
+            elif 0x01 <= byte <= 0x4b:
+                # this byte tells us how many bytes of data follows
+                # set the skip counter - don't check the next bytes for opcodes
+                skip_count = byte
+            # elif byte == 0x4c:
+                # nothing to do for 0x4c (OP_PUSHDATA1)
+                # nothing will be processed and
+                # the next byte will contain the number of bytes of data
+            elif byte == 0x4d:
+                # the next 2 bytes tells us how many bytes of data follows
+                # set the skip counter to 2, plus however many bytes are coming
+                skip_count = (2 +
+                              struct.unpack(
+                                '>H', self.bytes[byte_count+1:byte_count+3])[0]
+                              )
+                # set a flag to not display the next 2 bytes either.
+                print_skip = 2
+            elif byte == 0x4e:
+                # the next four bytes tells us how many bytes of data follows
+                # set the skip counter to 4, plus however many bytes are coming
+                skip_count = (4 +
+                              struct.unpack(
+                                '>I', self.bytes[byte_count+1:byte_count+5])[0]
+                              )
+                # # set a flag to not display the next 4 bytes either.
+                print_skip = 4
+            # else, just look up the opcode
+            elif byte in self.opcodes.values():
+                for key in self.opcodes:
+                    if self.opcodes[key] == byte:
+                        stack += key + ' '
+
+            byte_count += 1
+
+        return stack
