a13e25b714a2df14903c7d25848be6357fcdddb8
Mike M <marzig76@gmail.com>
2016-03-31 20:45:05 -0400

    added docstrings

--- tx_input.py
+++ tx_input.py
@@ -1,17 +1,32 @@
+"""
+This module contains a class for parsing a bitcoin tx inputs.
+
+Author: Mike Marzigliano
+"""
+
 from blockutil import *
 
-class tx_input(object):
 
+class tx_input(object):
+    """This class is for parsing bitcoin tx inputs."""
 
     def __init__(self, blockstream):
+        """
+        Initialize the class.
+
+        Parse the values for all fields in each tx input.
+
+        Arguemnts:
+        blockstream - the stream of data to parse
+        """
         self.prev_hash = parse_hash(blockstream)
         self.index = parse_int(blockstream, 4)
         self.script_bytes = compact_size(blockstream)
         self.sigscript = blockstream.read(self.script_bytes)
         self.sequence = parse_int(blockstream, 4)
 
-
     def __str__(self):
+        """Build and return a string representing tx input data."""
         return (
             '\nPrevious Hash:\t' + str(self.prev_hash) +
             '\nIndex:\t' + str(self.index) +
