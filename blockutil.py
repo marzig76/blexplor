69f5c01093ba3969e14878372462246176cd75e1
Mike M <marzig76@gmail.com>
2016-03-31 20:31:01 -0400

    added docstrings

--- blockutil.py
+++ blockutil.py
@@ -1,7 +1,22 @@
+"""
+This module contains functions to parse bitcoin blocks.
+
+Author: Mike Marzigliano
+"""
+
 import struct
 
 
 def parse_int(blockstream, bytes):
+    """
+    Parse an integer value from a data stream.
+
+    Based on the number of bytes, determine the appropriate struct format.
+
+    Arguments:
+    blockstream - the stream of data to parse
+    bytes - the number of bytes to read
+    """
     if bytes == 2:
         format = 'H'
     elif bytes == 4:
@@ -10,14 +25,32 @@ def parse_int(blockstream, bytes):
         format = 'Q'
     else:
         return False
+
     return struct.unpack(format, blockstream.read(bytes))[0]
 
 
 def parse_hash(blockstream):
+    """
+    Simply return the 32 byte hexidecimal hash from the stream.
+
+    Reverse the bytes before encoding it in hex.
+
+    Arguments:
+    blockstream - the stream of data to parse
+    """
     return blockstream.read(32)[::-1].encode("hex")
 
 
 def compact_size(blockstream):
+    """
+    Return a variable length parsed integer.
+
+    Read the appropriate number of bytes based on the values of the first byte.
+    Use the above parse_int function to do the work.
+
+    Arguments:
+    blockstream - the stream of data to parse
+    """
     size = ord(blockstream.read(1))
 
     if size < 0xfd:
