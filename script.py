2ee2b934f4385a84b2f5dd23f041ed5ddd9ac4bc
Mike Marzigliano <marzig76@gmail.com>
2016-05-13 17:44:45 -0400

    added interpreter for scriptsigs

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 """
-This module contains a class to interperet script OP Codes from a scriptpubkey.
+This module contains classes to interperet scriptpubkeys and scriptSigs.
 
 Author: Mike Marzigliano
 """
@@ -9,7 +9,7 @@ from opcode import opcode
 
 class script(object):
     """
-    This class is for interpereting bitcoin sript OP Codes.
+    This class is for interpereting bitcoin scriptPubKeys.
 
     Define the existing OP Codes by creating an instance of the op_code class.
     """
@@ -21,7 +21,7 @@ class script(object):
         Take the given bytes and call the interpereter.
 
         Arguemnts:
-        bytes - the data to interperet
+        bytes - the data to interperet (scriptPubKey)
         """
         self.bytes = bytes
         self.stack = self.interperet()
@@ -97,3 +97,28 @@ class script(object):
     def __str__(self):
         """Return a string representing the scriptpubkey."""
         return self.stack
+
+
+class scriptSig(object):
+    """This class is for interpereting bitcoin scriptSigs."""
+
+    signature = ''
+    pubkey = ''
+
+    def __init__(self, bytes):
+        """
+        Take the scriptSig and extract the signature and public key.
+
+        Arguemnts:
+        bytes - the data to interperet (scriptSig)
+        """
+        sigbytes_hex = bytes[:1].encode('hex')
+        sigbytes = int(sigbytes_hex, 16)
+        self.signature = bytes[1:sigbytes+1]
+
+        pos = sigbytes+1
+        pubkeybytes_hex = bytes[pos:pos+1].encode('hex')
+        if pubkeybytes_hex != '':
+            pubkeybytes = int(pubkeybytes_hex, 16)
+            pos += 1
+            self.pubkey = bytes[pos:pos+pubkeybytes]
