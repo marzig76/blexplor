9bd4a2c3cce406b3a1d394d3e4e8b7f43f809e54
Mike M <marzig76@gmail.com>
2016-06-28 21:27:22 -0400

    rolling back last commit

--- script.py
+++ script.py
@@ -40,7 +40,57 @@ class script(object):
         stack = ''
 
         # build the script
-        
+        for i in self.bytes:
+            byte = ord(i)
+
+            # Either find a corresponding opcode
+            # or read the number of appropriate bytes of data.
+            # If just reading bytes, skip checking them as opcodes
+            if byte == 0x6a:
+                # 0x6a (OP_RETURN) the rest of the script is just data.
+                # skip it all
+                skip_count = len(self.bytes)
+            elif skip_count > 0:
+                if print_skip == 0:
+                    stack += i.encode('hex')
+                else:
+                    print_skip -= 1
+                skip_count -= 1
+                if skip_count == 0:
+                    stack += ' '
+            elif 0x01 <= byte <= 0x4b:
+                # this byte tells us how many bytes of data follows
+                # set the skip counter - don't check the next bytes for opcodes
+                skip_count = byte
+            # elif byte == 0x4c:
+                # nothing to do for 0x4c (OP_PUSHDATA1)
+                # nothing will be processed and
+                # the next byte will contain the number of bytes of data
+            elif byte == 0x4d:
+                # the next 2 bytes tells us how many bytes of data follows
+                data_len = struct.unpack(
+                    '>H', self.bytes[byte_count+1:byte_count+3]
+                    )[0]
+                # set the skip counter to 2, plus however many bytes are coming
+                skip_count = (2 + data_len)
+                # set a flag to not display the next 2 bytes either.
+                print_skip = 2
+            elif byte == 0x4e:
+                # the next four bytes tells us how many bytes of data follows
+                data_len = struct.unpack(
+                    '>I', self.bytes[byte_count+1:byte_count+5]
+                    )[0]
+                # set the skip counter to 4, plus however many bytes are coming
+                skip_count = (4 + data_len)
+                # # set a flag to not display the next 4 bytes either.
+                print_skip = 4
+            # else, just look up the opcode
+            elif byte in self.opcodes.values():
+                for key in self.opcodes:
+                    if self.opcodes[key] == byte:
+                        stack += key + ' '
+
+            byte_count += 1
 
         return stack
 
